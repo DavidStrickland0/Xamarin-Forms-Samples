193e84c7a68aeb9562ee0b2c10061db094abb629
DavidStrickland0 <davidstrickland0@hotmail.com>
2015-09-29 11:28:57 -0400

    Added in Model and fixed some naming issues

---
+++ BoolToImageConvertor/BoolToImageConvertor/Model/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace BoolToImageConvertor
+{
+	public class Account : ContentPage
+	{
+		public Account ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
