9517c7521bdf3d07c8baf3cdb2411e42c48927a5
DavidStrickland0 <davidstrickland0@hotmail.com>
2015-10-01 03:31:29 -0400

    Updated MasterDetail to use Convertors

---
+++ MasterDetailChangeIcon/MasterDetailChangeIcon/Convertor/ImagePathConvertor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BoolToImageConvertor
+{
+	public class ImagePathConvertor : IValueConverter
+	{
+		public ImagePathConvertor()
+		{
+			System.Diagnostics.Debug.WriteLine("t");
+		}
+
+		#region IValueConverter implementation
+
+		public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value is bool)  {
+
+				if((bool)value == true)
+					return "noci.png";
+				else
+					return "icon.png";
+			}
+			return "";
+		}
+
+		public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+	}
+}
+
