54f6129765ef9f1a69a00748ef5b8bcf08044b19
David Strickland <davidstrickland0@hotmail.com>
2015-10-17 16:43:20 -0400

    Sample app testing a binding issue.
    https://forums.xamarin.com/discussion/54042/using-binding-with-value-converters

---
+++ ListViewBindingConverter/ListViewItemConverter/MyPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace ListViewItemConverter
+{
+	public partial class MyPage : ContentPage
+	{
+
+		public class Place
+		{
+			public string Name{ get; set;}
+			public string Date{ get; set;}
+			public DateTime Time{ get; set;}
+		}
+
+		public MyPage()
+		{
+			InitializeComponent ();
+			Places = new ObservableCollection<Place>();
+			Places.Add (new Place{ Name = "Name", Date = DateTime.Now.Date.ToString(), Time = DateTime.Now.ToLocalTime() });
+			Places.Add (new Place{ Name = "Name", Date = DateTime.Now.Date.ToString(), Time = DateTime.Now.ToLocalTime() });
+			Places.Add (new Place{ Name = "Name", Date = DateTime.Now.Date.ToString(), Time = DateTime.Now.ToLocalTime() });
+			Places.Add (new Place{ Name = "Name", Date = DateTime.Now.Date.ToString(), Time = DateTime.Now.ToLocalTime() });
+			Places.Add (new Place{ Name = "Name", Date = DateTime.Now.Date.ToString(), Time = DateTime.Now.ToLocalTime() });
+			Places.Add (new Place{ Name = "Name", Date = DateTime.Now.Date.ToString(), Time = DateTime.Now.ToLocalTime() });
+			BindingContext = this;
+		}
+		public ObservableCollection<Place> Places { get; set; }
+	}
+}
+
---
+++ ListViewBindingConverter/ListViewItemConverter/distanceconvertor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace ListViewItemConverter
+{
+	class DistanceConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string result;
+			//value is set to null
+			if (value == null)
+				result = "Im Null";
+			else
+				result = "Im Not";
+
+			return result;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
+
